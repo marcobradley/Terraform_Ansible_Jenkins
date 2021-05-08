f4caf6585bf6f9816f9876bbf21e2708dd23fff0
marcobradley <mbradley@complysci.com>
2021-05-08 16:06:00 -0400

    Adding the ALB to the terraform scripts

--- deploy_iac_tf_ansible/outputs.tf
+++ deploy_iac_tf_ansible/outputs.tf
@@ -8,3 +8,8 @@ output "Jenkins-Worker-Public-IPs" {
     instance.id => instance.public_ip
   }
 }
+
+#Add LB DNS name to outputs.tf
+output "LB-DNS-NAME" {
+  value = aws_lb.application-lb.dns_name
+}
